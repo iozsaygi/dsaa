9bd3b5ff154908a4f4e8b84c868e33548334a3e9
İsmail Özsaygı <devozsaygi@gmail.com>
2023-11-19 17:55:25 +0300

    Added basic source files for queue

---
+++ include/queue.h
@@ -0,0 +1,9 @@
+// https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+// ------------------------- TESTS -------------------------
+// ---------------------------------------------------------
+
+#endif// !QUEUE_H
