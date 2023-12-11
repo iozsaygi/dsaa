48fb9e29e8ff4ea45f2dbf789a1146dfff058be5
İsmail Özsaygı <devozsaygi@gmail.com>
2023-12-11 20:32:15 +0300

    Added hashing function

--- include/hash_table.h
+++ include/hash_table.h
@@ -3,6 +3,32 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
+#include <stdlib.h>
+
+// Assuming keys are raw strings.
+typedef struct {
+    char* data;
+} key_t;
+
+// Assuming values are integers.
+typedef struct {
+    int data;
+} val_t;
+
+typedef struct {
+    int* data;
+    size_t size;
+} ht_t;
+
+// Allocates the hash table.
+ht_t* ht_allocate(size_t size);
+
+// Calculates the hash of given string.
+unsigned int ht_hash(const ht_t* ht, key_t key);
+
+// Clears allocated resources for hash table.
+void ht_free(ht_t* ht);
+
 // ------------------------- TESTS -------------------------
 void ht_test_0();
 // ---------------------------------------------------------
--- src/hash_table.c
+++ src/hash_table.c
@@ -1,6 +1,47 @@
 #include "hash_table.h"
+#include <assert.h>
+#include <stdio.h>
+
+ht_t* ht_allocate(size_t size) {
+    ht_t* ht = (ht_t*) malloc(sizeof(int) * size);
+    assert(ht != NULL);
+
+    ht->size = size;
+
+    return ht;
+}
+
+unsigned int ht_hash(const ht_t* ht, key_t key) {
+    assert(ht != NULL);
+
+    unsigned int itr = 0;
+    const unsigned int prime = 37;
+
+    for (size_t i = 0; key.data[i] != '\0'; i++) {
+        itr = prime * itr + key.data[i];
+    }
+
+    return itr % ht->size;
+}
+
+void ht_free(ht_t* ht) {
+    free(ht->data);
+    ht->size = 0;
+    free(ht);
+}
 
 // ------------------------- TESTS -------------------------
 void ht_test_0() {
+    ht_t* ht = ht_allocate(10);
+
+    // Test out hash calculations.
+    key_t first = {.data = "first_key#1"};
+    key_t second = {.data = "second_key#2"};
+    key_t third = {.data = "third_key#3"};
+    printf("%u\n", ht_hash(ht, first));
+    printf("%u\n", ht_hash(ht, second));
+    printf("%u\n", ht_hash(ht, third));
+
+    ht_free(ht);
 }
 // ---------------------------------------------------------
