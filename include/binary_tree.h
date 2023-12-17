c73271218a27e00bc79c34ed39776be8f702a34d
İsmail Özsaygı <devozsaygi@gmail.com>
2023-12-17 15:20:13 +0300

    Add definitions for binary trees

--- include/binary_tree.h
+++ include/binary_tree.h
@@ -3,6 +3,18 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+// Actual node structure for the binary tree.
+struct btn {
+    int ctx;
+    struct btn* left;
+    struct btn* right;
+};
+
+// Actual tree that stores root node.
+typedef struct {
+    struct btn* root;
+} bt_t;
+
 // ------------------------- TESTS -------------------------
 void bt_test_0();
 // ---------------------------------------------------------
