7917c2969f0fe701e63a8e75e18345cd1f8bb197
İsmail Özsaygı <devozsaygi@gmail.com>
2023-12-29 22:54:57 +0300

    Added basic operations for min heap

--- include/min_heap.h
+++ include/min_heap.h
@@ -1,6 +1,37 @@
 #ifndef MIN_HEAP_H
 #define MIN_HEAP_H
 
+#include <stdlib.h>
+
+// Core struct to represent min-heap.
+typedef struct {
+
+    // Maximum numbers of elements allowed.
+    size_t capacity;
+
+    // Current size of the min-heap.
+    size_t length;
+
+    // Actual pointer to store array.
+    int* data;
+
+} mh_t;
+
+// Allocates required resources and returns pointer to min-heap.
+mh_t* minHeap_allocate(size_t capacity);
+
+// Returns the smallest element from min-heap.
+int minHeap_peek(const mh_t* mh);
+
+// Calculates the index of the parent node for the node with given index.
+size_t minHeap_findParentNodeIndex(size_t index);
+
+// Calculates the index of the left node for the node with given index.
+size_t minHeap_findLeftNodeIndex(size_t index);
+
+// Calculates the index of the right node for the node with given index.
+size_t minHeap_findRightNodeIndex(size_t index);
+
 // ------------------------- TESTS -------------------------
 void min_heap_test_0();
 // ---------------------------------------------------------
--- src/min_heap.c
+++ src/min_heap.c
@@ -1,4 +1,38 @@
 #include "min_heap.h"
+#include <assert.h>
+
+mh_t* minHeap_allocate(size_t capacity) {
+    assert(capacity > 0);
+
+    // Allocate the min-heap itself.
+    mh_t* mh = (mh_t*) malloc(sizeof(mh_t));
+    assert(mh != NULL);
+
+    mh->capacity = capacity;
+    mh->length = 0;
+
+    // Allocate for the internal array.
+    mh->data = (int*) calloc(capacity, sizeof(int));
+
+    return mh;
+}
+
+int minHeap_peek(const mh_t* mh) {
+    assert(mh != NULL);
+    return mh->data[0];
+}
+
+size_t minHeap_findParentNodeIndex(size_t index) {
+    return (index - 1) / 2;
+}
+
+size_t minHeap_findLeftNodeIndex(size_t index) {
+    return (index * 2) + 1;
+}
+
+size_t minHeap_findRightNodeIndex(size_t index) {
+    return (index * 2) + 2;
+}
 
 // ------------------------- TESTS -------------------------
 void min_heap_test_0() {
