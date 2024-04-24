58e2db7f69b171ffc45ba726e5f6337b443a6273
İsmail Özsaygı <devozsaygi@gmail.com>
2024-04-24 21:54:38 +0300

    Added API to create nodes for BFS

--- include/breadth_first_search.h
+++ include/breadth_first_search.h
@@ -3,6 +3,23 @@
 #ifndef BREADTH_FIRST_SEARCH_H
 #define BREADTH_FIRST_SEARCH_H
 
+struct bfs_node {
+    int data;
+    struct bfs_node* next;
+};
+
+struct bfs_list {
+    struct bfs_node* head;
+};
+
+struct bfs_graph {
+    int nodeCount;
+    struct bfs_list* list;
+};
+
+// Creates node to be used during search.
+struct bfs_node* bfs_createNode(int data);
+
 // ------------------------- TESTS -------------------------
 void bfs_test_0();
 // ---------------------------------------------------------
--- src/breadth_first_search.c
+++ src/breadth_first_search.c
@@ -1,4 +1,12 @@
 #include "breadth_first_search.h"
+#include <stdlib.h>
+
+struct bfs_node* bfs_createNode(int data) {
+    struct bfs_node* node = (struct bfs_node*) malloc(sizeof(struct bfs_node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
 
 // ------------------------- TESTS -------------------------
 void bfs_test_0() {
